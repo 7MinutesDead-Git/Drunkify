122f82f74ce89573e92fd776dee73cee0ef7cc59
7MinutesDead-Git <7minutesdead@gmail.com>
2022-04-25 13:39:57 -0500

    - Prevent duplicate drinks from rendering. - Refactor promise "then" chains to await syntax. - Refactor functions for more descriptive names. - Add documentation comments to functions. - Clean up code formatting.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
+// -------------------------------------------------------------
+// Variables.
 const button = document.querySelector("button")
 const input = document.querySelector("input")
 const errorSpan = document.querySelector(".error-text")
 const cocktailList = document.querySelector('.cocktails')
 const searchBar = document.querySelector('.searchbar')
+
+let drinkButtons
+let drinksOnDisplay = {}
+
 window.oldScroll = window.scrollY
 
 // -------------------------------------------------------------
+// Create a promise to resolve after a given delay in ms.
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 // -------------------------------------------------------------
+// Setup user input event listeners.
 function setupListeners() {
     button.addEventListener('click', () => {
-        getFetch()
+        getDrinks()
     })
     input.addEventListener('keypress', e => {
         if (e.key === 'Enter')
-            getFetch()
+            getDrinks()
     })
     window.onscroll = (e) => {
         toggleOpacityOnScroll(searchBar)
@@ -25,21 +33,25 @@ function setupListeners() {
 }
 
 // -------------------------------------------------------------
+// Setup click event listeners for the drink buttons.
 function setupDrinkListeners() {
-    const drinkButtons = document.querySelectorAll('.drink')
+    drinkButtons = document.querySelectorAll('.drink')
     drinkButtons.forEach(button => {
         button.addEventListener('click', (e) => {
+            // Search by ingredient just by clicking on the ingredient link.
             if (e.target.tagName === 'A') {
                 input.value = e.target.innerText
-                getFetch(e.target.innerText)
+                getDrinks(input.value)
             }
             else
-                toggleFocus(button)
+                toggleDrinkFocus(button)
         })
     })
 }
 
 // -------------------------------------------------------------
+// Show and hide an element when scrolling.
+// ie: Hide the search bar when scrolling down, but reveal it when trying to scroll back up to it.
 function toggleOpacityOnScroll(element) {
     if (window.oldScroll > window.scrollY) {
         element.classList.remove('hidden')
@@ -50,7 +62,9 @@ function toggleOpacityOnScroll(element) {
 }
 
 // -------------------------------------------------------------
-async function toggleFocus(drink) {
+// Enter and exit focus on a drink button.
+// Also scrolls to the drink button's current location on focus and unfocus.
+async function toggleDrinkFocus(drink) {
     drink.classList.toggle('viewing')
     drink.scrollIntoView({
         behavior: 'smooth',
@@ -59,64 +73,74 @@ async function toggleFocus(drink) {
 }
 
 // -------------------------------------------------------------
+// Sanitize the user's search input by only allowing alphanumeric characters and spaces between words.
 function sanitizeInput(stringInput) {
-    let result = stringInput.trim()
-    return result
+    return stringInput.trim().replace(/[^a-z\d\s]/gi,'')
 }
 
 // -------------------------------------------------------------
-async function getFetch(choice = null) {
-    clearList()
+// Get the drinks from the API and display them on screen.
+// Clears any previously existing drinks on screen.
+async function getDrinks(choice = null) {
+    clearCocktailList()
     if (!choice)
         choice = sanitizeInput(input.value)
+
     const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`
     const ingredientURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${choice}`
-    getDrinksByIngredient(ingredientURL)
-    getDrinksByName(drinkURL)
-    // A stopgap to allow time for elements to render before attaching listeners and revealing.
+    console.log(choice)
+
+    // TODO Setup promises here and get rid of arbitrary wait time.
+    await fetchDrinksByName(drinkURL)
+    await fetchDrinksByIngredient(ingredientURL)
     await wait(1000)
-    setupDrinkListeners()
-    revealDrinks()
+    await setupDrinkListeners()
+    await revealDrinks()
 }
 
 // -------------------------------------------------------------
-function getDrinksByName(url) {
-    fetch(url)
-        .then(res => {
-            renderError(res.status)
-            return res.json()
-        })
-        .then(data => {
-            console.log(data)
-            renderDrinks(data)
-        })
-        .catch(err => {
-            console.log(`Caught this error: ${err}`)
-        })
+// Retrieve drink data by name and render them on the screen.
+async function fetchDrinksByName(url) {
+    try {
+        console.log('Fetching drinks by name')
+        const response = await fetch(url)
+        await renderError(response.status)
+
+        const data = await response.json()
+        console.log(data)
+        await renderDrinks(data)
+    }
+    catch (err) {
+        console.log(`Caught this error: ${err}`)
+    }
 }
 
 // -------------------------------------------------------------
-function getDrinksByIngredient(idURL) {
-    fetch(idURL)
-        .then(res => {
-            renderError(res.status)
-            return res.json()
-        })
-        .then(data => {
-            // Since ingredients don't return full drink details,
-            // we'll need to do a second fetch by the IDs we find.
-            for (const id of data['drinks']) {
-                const idNumber = id['idDrink']
+// Retrieve drink data by ingredient, then search by ID with name function to get .
+async function fetchDrinksByIngredient(idURL) {
+    try {
+        console.log('Fetching drinks by ingredient...')
+        const response = await fetch(idURL)
+        await renderError(response.status)
+
+        const data = await response.json()
+
+        for (const drink of data['drinks']) {
+            if (!(drinkExists(drink))) {
+                const idNumber = drink['idDrink']
                 const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idNumber}`
-                getDrinksByName(drinkURL)
+                fetchDrinksByName(drinkURL)
             }
-        })
-        .catch(err => {
-            console.log(`Caught this error: ${err}`)
-        })
+        }
+    }
+    catch (err) {
+        console.log(`Caught this error: ${err}`)
+    }
 }
 
 // -------------------------------------------------------------
+// Function for handling and rendering error codes.
+// For now, it's a catchall for 404 when the given drink doesn't exist.
 function renderError(code) {
     if (code === 404){
         errorSpan.innerHTML = `Couldn't find a drink or ingredient named ${input.value} :(`
@@ -127,17 +151,24 @@ function renderError(code) {
 }
 
 // -------------------------------------------------------------
-async function renderDrinks(data) {
+// Create each drink block and append them to the cocktail list to be displayed.
+function renderDrinks(data) {
     if (data['drinks']) {
         for (const drink of data['drinks']) {
-            cocktailList.appendChild(createDrinkBlock(drink))
+            if (!(drinkExists(drink))) {
+                drinksOnDisplay[drink['strDrink']] = true
+                cocktailList.appendChild(createDrinkBlock(drink))
+            }
         }
     } else {
         renderError(404)
     }
+    console.log(`${Object.keys(drinksOnDisplay).length} drinks on display.`)
+    console.log(`${cocktailList.childElementCount} drinks in the list.`)
 }
 
 // -------------------------------------------------------------
+// Gradually reveal each drink in the list.
 async function revealDrinks() {
     for (const drink of document.querySelectorAll('.drink')) {
         await wait(200)
@@ -146,6 +177,17 @@ async function revealDrinks() {
 }
 
 // -------------------------------------------------------------
+// Check if the given drink is already on the page.
+function drinkExists(drink) {
+    const exists = drink['strDrink'] in drinksOnDisplay
+    if (exists)
+        console.log(`${drink['strDrink']} already exists on the page. Skipping.`)
+    return exists
+}
+
+// -------------------------------------------------------------
+// Create the HTML structure for a drink block.
+// This inclludes the name, image, ingredients, and instructions.
 function createDrinkBlock(data) {
     const drink = document.createElement('li')
     const name = document.createElement('h3')
@@ -169,6 +211,8 @@ function createDrinkBlock(data) {
 }
 
 // -------------------------------------------------------------
+// Create the HTML structure for the ingredients list.
+// Match ingredients with ingredient measure amounts, and remove empty entries.
 function getIngredients(drink) {
     const ingredients = document.createElement('ul')
     ingredients.classList.add('ingredients')
@@ -179,10 +223,10 @@ function getIngredients(drink) {
     // TODO: If the API stops returning ingredients before measurements, we'll need to refactor this.
     for (const key in drink) {
         const suffix = key.charAt(key.length - 1)
-        if (key.includes('Ingredient') && drinkKeyIsValid(drink, key)) {
+        if (key.includes('Ingredient') && drinkPropertyIsValid(drink, key)) {
             measurementPairs[suffix] = drink[key]
         }
-        if (key.includes('Measure') && drinkKeyIsValid(drink, key) && measurementPairs[suffix].length > 0) {
+        if (key.includes('Measure') && drinkPropertyIsValid(drink, key) && measurementPairs[suffix].length > 0) {
             const measurement = drink[key]
             const ingredient = document.createElement('li')
             ingredient.innerHTML = `<a>${measurementPairs[suffix]}</a>: ${measurement}`
@@ -194,29 +238,30 @@ function getIngredients(drink) {
 }
 
 // -------------------------------------------------------------
-function drinkKeyIsValid(drink, key) {
+// Check if given drink property key is not blank and not null.
+function drinkPropertyIsValid(drink, key) {
     return drink[key] !== null && drink[key].length > 0
 }
 
 // -------------------------------------------------------------
-function clearList() {
+// Reset the page to its empty state.
+function clearCocktailList() {
     cocktailList.innerHTML = ''
+    drinksOnDisplay = {}
 }
 
 // -------------------------------------------------------------
+// Structure the instructions into paragraph elements per sentence.
 function formatInstructions(instructions) {
     let result = ''
     if (instructions) {
         const array = instructions.split('.')
-        for (const instruction of array) {
-            if (instruction.length > 0) {
+        for (const instruction of array)
+            if (instruction.length > 0)
                 result += `<p>${instruction}.</p>`
-            }
-        }
     }
     return result
 }
-
 
 // -------------------------------------------------------------
 window.onload = setupListeners
