e8ff0ab319e69c38a92b54d5fe944ae989ac3d94
7MinutesDead-Git <7minutesdead@gmail.com>
2022-08-17 17:31:27 -0500

    Extract api-errors and drink classes to separate files. (...) - Move drink-related static functions to drink.js. - Add async fade out effect when screen is cleared.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+import Drink from "./drink.js"
+import APIErrors from "./api-errors.js"
 // -------------------------------------------------------------
-// Variables.
 // TODO: Continue to encapsulate as much of these variables as possible.
-// -------------------------------------------------------------
 let searchButton
 let clearButton
 let searchInput
@@ -12,130 +12,15 @@ let drinkButtons
 let drinksOnDisplay
 let loadingIcon
 let suggestions
-
 let fetchedDrinks = []
+
 // TODO: Base fade on screen width (so the edge is completely faded).
 const SEARCH_HISTORY_LIMIT = 5
 const DRINK_REVEAL_SPEED = 180  // milliseconds, lower is faster
 const AUTOSCROLL_DELAY = 75
 
-// -------------------------------------------------------------
-// Classes.
-// -------------------------------------------------------------
-
-class Drink {
-    constructor(apiData) {
-        this.data = apiData
-        this.instructionsData = apiData['strInstructions']
-        this.ingredientsElement = getIngredients(apiData)
-
-        this.drinkListItem = document.createElement('li')
-        this.nameHeader = document.createElement('h3')
-        this.instructionsDiv = document.createElement('div')
-        this.imageElement = document.createElement('img')
-        this.heart = this.setupHeart()
-        // TODO: Check localStorage if drink matches a favorite.
-        this.favorited = false
-
-        this.childElements = [
-            this.imageElement,
-            this.nameHeader,
-            // this.heart,
-            this.ingredientsElement,
-            this.instructionsDiv
-        ]
-        this.setupDrink()
-    }
-    getFormattedInstructions() {
-        let result = ''
-        if (this.instructionsData) {
-            const array = this.instructionsData.split('.')
-            for (const instruction of array)
-                if (instruction.length > 0)
-                    result += `<p>${instruction}.</p>`
-        }
-        return result
-    }
-    addDrinkStyling() {
-        this.drinkListItem.classList.add('drink')
-    }
-    addInstructions() {
-        this.instructionsDiv.classList.add('instructions')
-        this.instructionsDiv.innerHTML = this.getFormattedInstructions()
-    }
-    appendDrinkElements() {
-        for (const element of this.childElements)
-            this.drinkListItem.appendChild(element)
-    }
-    setupDrinkImage() {
-        this.imageElement.classList.add('hidden')
-
-        // To prevent seeing the image loading in before it's fully loaded.
-        this.imageElement.onload = () => {
-            this.imageElement.classList.remove('hidden')
-            this.nameHeader.classList.remove('loading')
-            this.nameHeader.innerHTML = this.data['strDrink']
-        }
-        this.imageElement.src = this.data['strDrinkThumb']
-    }
-    setDrinkName() {
-        this.nameHeader.innerHTML = "Loading..."
-        this.nameHeader.classList.add('loading')
-    }
-    setupDrink() {
-        this.addDrinkStyling()
-        this.addInstructions()
-        this.setDrinkName()
-        this.setupDrinkImage()
-        this.appendDrinkElements()
-    }
-    getDrinkElement() {
-        return this.drinkListItem
-    }
-    setupHeart() {
-        // TODO: Construct heart favorite icon
-        const element = document.createElement('div')
-        element.innerHTML = svgHeart
-        // element.type = 'checkbox'
-        // if (this.favorited)
-        //     element.checked = true
-        return element
-    }
-}
-
-// Handles errors received or implied by the API responses.
-// Renders the error message to the DOM when user needs to see it (say if no drinks were found).
-class APIErrors {
-    errors = {}
-    errorSpan = document.querySelector(".error-text")
-
-    storeError(error) {
-        if (error !== 200)
-            this.errors[error] = true
-    }
-    renderErrors() {
-        // We don't need to show errors to the user received by multiple API endpoints if the end result
-        // is that we still found some drinks with some of the requests.
-        if (cocktailList.childElementCount === 0) {
-            for (const error in this.errors)
-                this.errorSpan.innerHTML += `<p>${error}</p>`
-        } else {
-            for (const error in this.errors)
-                console.log(error)
-        }
-    }
-    clearErrors() {
-        this.errors = []
-        this.errorSpan.innerHTML = ''
-    }
-}
-
 
 // -------------------------------------------------------------
-// Static functions.
-// TODO: Encapsulate more of this behavior in new classes.
-// -------------------------------------------------------------
-
 // Create a promise to resolve after a given delay in ms.
 // We can use this for animation-like effects or arbitrary delays.
 function wait(ms) {
@@ -348,48 +233,20 @@ function drinkExists(drink) {
 }
 
 
-// Create the HTML structure for the ingredients list.
-// Match ingredients with ingredient measure amounts, and remove empty entries.
-function getIngredients(drink) {
-    const ingredients = document.createElement('ul')
-    ingredients.classList.add('ingredients')
-    const measurementPairs = {}
-    // To match the ingredients with their measurements, we can check the last character of the key name
-    // since each measurement and ingredient name have a matching number suffix.
-    // This works so long as there isn't more than 10 ingredients.
-    // TODO: If the API ever stops returning ingredients before measurements in the future, we'll need to refactor this.
-    for (const key in drink) {
-        const suffix = key.charAt(key.length - 1)
-        if (key.includes('Ingredient') && drinkPropertyIsValid(drink, key)) {
-            measurementPairs[suffix] = drink[key]
-        }
-        if (key.includes('Measure') && drinkPropertyIsValid(drink, key) && measurementPairs[suffix].length > 0) {
-            const measurement = drink[key]
-            const ingredient = document.createElement('li')
-            ingredient.innerHTML = `<a>${measurementPairs[suffix]}</a>: ${measurement}`
-            ingredient.classList.add('ingredient')
-            ingredients.appendChild(ingredient)
-        }
-    }
-    // If there are no ingredients, add a button for submitting new ingredients.
-    if (ingredients.children.length === 0) {
-        ingredients.innerHTML = '<li>No ingredients listed. Submit some!</li>'
-        const submitIngredientButton = document.createElement('button')
-        submitIngredientButton.innerHTML = 'Submit ingredient'
-        ingredients.appendChild(submitIngredientButton)
-    }
-    return ingredients
-}
-
-// Check if given drink property key is not blank and not null.
-function drinkPropertyIsValid(drink, key) {
-    return drink[key] !== null && drink[key].length > 0
-}
-
 // Reset the page to its empty state.
-function clearScreen() {
+async function clearScreen() {
+    fadeOutAllDrinks()
+    await wait(DRINK_REVEAL_SPEED)
     cocktailList.innerHTML = ''
     drinksOnDisplay = {}
+}
+
+// Fade out all drinks on the page.
+function fadeOutAllDrinks() {
+    for (const drink of document.querySelectorAll('.drink')) {
+        drink.style.boxShadow = 'none'
+        drink.style.opacity = '0'
+    }
 }
 
 // Add search term to local history.
@@ -465,8 +322,6 @@ function cycleSuggestions() {
 
 
 // -------------------------------------------------------------
-// Start here.
-// -------------------------------------------------------------
 window.onload = () => {
     // Add new property to window object for the sake of keeping track of previous scroll position.
     window.oldScroll = window.scrollY
@@ -477,7 +332,7 @@ window.onload = () => {
     cocktailList = document.querySelector('.cocktails')
     searchSection = document.querySelector('.search-section')
     loadingIcon = document.querySelector('.lds-ellipsis')
-    errors = new APIErrors()
+    errors = new APIErrors(cocktailList)
     drinksOnDisplay = {}
     setupListeners()
     updateSearchHistoryDisplay(getSearchHistory())
