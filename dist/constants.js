de3b70234217de8fc10328d5c78d1e7feeffbf49
7MinutesDead-Git <7minutesdead@gmail.com>
2022-09-19 17:28:35 -0500

    Increase drink reveal speed

--- dist/constants.js
+++ dist/constants.js
@@ -3,7 +3,7 @@ export class UISettings {
 // TODO: Base fade on screen width (so the edge is completely faded).
 UISettings.searchHistoryLimit = 5;
 // milliseconds, lower is faster
-UISettings.drinkRevealSpeed = 180;
+UISettings.drinkRevealSpeed = 140;
 UISettings.autoScrollDelay = 75;
 // For letting the user know the search results are already present.
 UISettings.backgroundFlashDuration = 300;
--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export abstract class UISettings {
     // TODO: Base fade on screen width (so the edge is completely faded).
     public static searchHistoryLimit: number = 5
     // milliseconds, lower is faster
-    public static drinkRevealSpeed = 180
+    public static drinkRevealSpeed = 140
     public static autoScrollDelay = 75
     // For letting the user know the search results are already present.
     public static backgroundFlashDuration = 300
